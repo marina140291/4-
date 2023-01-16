cc5d6ac4f4b8fbbd873c757bea20788566b2f515
marina140291 <marina140291@mail.ru>
2023-01-16 23:12:23 +0300

    27

---
+++ дом работа/27/27.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine();
+int sum = 0;
+Char [] a = n.ToCharArray(); // Преобразовали в массив
+for (int i = 0; i < n.Length; i++)
+{
+int b = Convert.ToInt32(n[i] - '0');   
+sum = sum + b;
+} 
+Console.WriteLine(sum);
