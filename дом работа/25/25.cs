0813675bfb598a0cc89fe3a4ede94e7a6bd87c9f
marina140291 <marina140291@mail.ru>
2023-01-16 23:09:32 +0300

    25

---
+++ дом работа/25/25.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) 
+//и возводит число А в натуральную степень В.
+
+Console.WriteLine ( "Введите число A: " );
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ( "Введите число B: " );
+double B = Convert.ToDouble(Console.ReadLine());
+
+double N = Math.Pow(A, B);
+Console.Write(N);
+
